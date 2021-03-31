8bab43e86e9763d1c4d2e1d88700711a7b43495e
shinji0320 <shinji.blue.one@gmail.com>
2021-04-01 08:58:57 +0900

    1-2

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-  },
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     publicPath: 'dist/',
-  },
-  devServer: {
-    hot: true,
-    open: true,
   }
 }
