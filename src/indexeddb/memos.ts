659295406b1a9b0ab75b8a36158a9a283ca60479
shinji0320 <shinji.blue.one@gmail.com>
2021-04-02 17:59:43 +0900

    4-4

--- src/indexeddb/memos.ts
+++ src/indexeddb/memos.ts
@@ -10,13 +10,24 @@ const database = new Dexie('markdown-editor')
 database.version(1).stores({ memos: '&datetime' })
 const memos: Dexie.Table<MemoRecord, string> = database.table('memos')
 
+const NUM_PER_PAGE: number = 10
+
+export const getMemoPageCount = async (): Promise<number> => {
+  const totalCount = await memos.count()
+  const pageCount = Math.ceil(totalCount / NUM_PER_PAGE)
+  return pageCount > 0 ? pageCount : 1
+}
+
 export const putMemo = async (title: string, text: string): Promise<void> => {
   const datetime = new Date().toISOString()
   await memos.put({ datetime, title, text })
 }
 
-export const getMemos = (): Promise<MemoRecord[]> => {
+export const getMemos = (page: number): Promise<MemoRecord[]> => {
+  const offset = (page - 1) * NUM_PER_PAGE
   return memos.orderBy('datetime')
               .reverse()
+              .offset(offset)
+              .limit(NUM_PER_PAGE)
               .toArray()
 }
--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -6,6 +6,7 @@ import {
 import styled from 'styled-components'
 import { Header } from '../components/header'
 import {
+  getMemoPageCount,
   getMemos,
   MemoRecord,
 } from '../indexeddb/memos'
@@ -20,12 +21,13 @@ const HeaderArea = styled.div`
 `
 
 const Wrapper = styled.div`
-  bottom: 0;
+  bottom: 3rem;
   left: 0;
   position: fixed;
   right: 0;
   top: 3rem;
   padding: 0 1rem;
+  overflow-y: scroll;
 `
 
 const Memo = styled.button`
@@ -37,33 +39,68 @@ const Memo = styled.button`
     margin: 1rem 0;
     text-align: left;
   `
+const Paging = styled.div`
+  bottom: 0;
+  height: 3rem;
+  left: 0;
+  line-height: 2rem;
+  padding: 0.5rem;
+  position: fixed;
+  right: 0;
+  text-align: center;
+`
 
-  const MemoTitle = styled.div`
-    font-size: 1rem;
-    margin-bottom: 0.5rem;
-  `
+const PagingButton = styled.button`
+  background: none;
+  border: none;
+  display: inline-block;
+  height: 2rem;
+  padding: 0.5rem 1rem;
 
-  const MemoText = styled.div`
-    font-size: 0.85rem;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  `
-
-
-
-  interface Props {
-    setText: (text: string) => void
+  &:disabled {
+    color: silver;
   }
+`
+
+const MemoTitle = styled.div`
+  font-size: 1rem;
+  margin-bottom: 0.5rem;
+`
+
+const MemoText = styled.div`
+  font-size: 0.85rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
 
 
-  export const History: React.FC<Props> = (props) => {
-    const { setText } = props
-    const [memos, setMemos] = useState<MemoRecord[]>([])
-    const history = useHistory()
-    useEffect(() => {
-      getMemos().then(setMemos)
-    }, [])
+
+interface Props {
+  setText: (text: string) => void
+}
+
+
+export const History: React.FC<Props> = (props) => {
+  const { setText } = props
+  const [memos, setMemos] = useState<MemoRecord[]>([])
+  const [page, setPage] = useState(1)
+  const [maxPage, setMaxPage] = useState(1)
+  const history = useHistory()
+  useEffect(() => {
+    getMemos(1).then(setMemos)
+    getMemoPageCount().then(setMaxPage)
+  }, [])
+
+  const canNextPage: boolean = page < maxPage
+    const canPrevPage: boolean = page > 1
+    const movePage = (targetPage: number) => {
+      if (targetPage < 1 || maxPage < targetPage) {
+        return
+      }
+      setPage(targetPage)
+      getMemos(targetPage).then(setMemos)
+    }
 
   return (
     <>
@@ -88,6 +125,21 @@ const Memo = styled.button`
         </Memo>
       ))}
       </Wrapper>
+      <Paging>
+          <PagingButton
+            onClick={() => movePage(page - 1)}
+            disabled={!canPrevPage}
+          >
+            ＜
+          </PagingButton>
+          {page} / {maxPage}
+          <PagingButton
+            onClick={() => movePage(page + 1)}
+            disabled={!canNextPage}
+          >
+            ＞
+          </PagingButton>
+        </Paging>
     </>
   )
 }
